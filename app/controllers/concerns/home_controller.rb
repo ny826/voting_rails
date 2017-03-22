a75a24cc1dc121e76d20b08629fffa902c1e8e5c
Naveen yadav <yadavnaveen826@gmail.com>
2017-03-23 01:17:20 +0530

    logout bug

--- app/controllers/concerns/home_controller.rb
+++ app/controllers/concerns/home_controller.rb
@@ -9,29 +9,23 @@ def rajiv
 
 	rajiv=70116403215
 	RajivRanjan.create(:voter=>current_user.id, :self=>rajiv)
-	return redirect_to '/logout'
+	return redirect_to '/';
 end
 
 def neil
    neil=40816403215
    NeilTyagi.create(:voter=>current_user.id, :self=>neil)
-   return redirect_to '/logout'
+	return redirect_to '/';
 
 end
 
 def result
-render :layout => false
 
 end
 
 def see
-	render :layout => false
 
 end
-
-
-
-
 
 end
 
